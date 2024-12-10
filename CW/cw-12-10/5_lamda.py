40bf3e24c9f3005436d17992a3ae5abae3a82ea0
Vladyslav Karlinskyi <91985923+karlvava16@users.noreply.github.com>
2024-12-10 10:23:45 +0200

    cw 12/10

--- CW/cw-12-10/5_lamda.py
+++ CW/cw-12-10/5_lamda.py
@@ -1,15 +1,26 @@
 # Функціональні вирази, lambda.
-def oper(lam) -> int:
-    return lam(10, 20)
+lam2 = None
+y = 100
 
-def main():
-    print("+", oper(lambda x, y: x + y))
-    print("-", oper(lambda x, y: x - y))
-    lam1 = lambda x: print(x)
-    lam1(10)
+def create_lam2() :
+    global lam2
+    y = 200
+    lam2 = lambda x : x + y # захват (замикання) здійснено для локальної змінної у
+
+def oper( lam ) -> int :
+    return lam( 10, 20 )
+
+def main() :
+    print( "+", oper( lambda x, y : x + y ) )
+    print( "-", oper( lambda x, y : x - y ) )
+    lam1 = lambda x : print(x)
+    lam1( 10 )
     # IIFE Immediately Invoked Functional Expression
-    ( lambda: print('IIFE') )()
-    print('*', oper(mul))
+    ( lambda : print('IIFE') )()
+    # CyMicHiCTb lambda Ta def
+    print('*', oper( mul ) )
+    y = 300
+    print( lam2(0) )
 
 
 def mul(x, y):
