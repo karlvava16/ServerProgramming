6ee62c0b50118c75d4d4452f0e25ba361b5a740a
Vladyslav Karlinskyi <91985923+karlvava16@users.noreply.github.com>
2024-12-12 09:06:37 +0200

    cw 12/12

--- CW/CW-12-11/8_oop.py
+++ CW/CW-12-11/8_oop.py
@@ -3,17 +3,30 @@
 class Point:
     x = 0                              # оголошення класу
     y = 0                              #
-
     def __init__(self, x, y):
         self.x = x
         self.y = y
 
+    def __str__(self):
+        return "(%f, %f)" % (self.x, self.y)
+
+    def __repr__(self):
+        return "<%s> (%f, %f)" % (__class__.__name__, self.x, self.y)
+
+    def __add__(self, other):
+        return Point(self.x + other.x, self.y + other.y)
+
+
                                        #
                                        #
 def main():                            #
-    p1 = Point()                       # Створення об'єкту - оператор new не вживається
+    p1 = Point(1, 2)                       # Створення об'єкту - оператор new не вживається
     print(p1.x, p1.y)                  #
     Point.x = 10                       # оголошення полів у класі дозволяє статичний доступ
     print(p1.x, p1.y)                  # 5 10
+    p2 = Point(10, 20)
+    print(p2)
+    print(repr(p2))
+
 
 if __name__ == '__main__': main()
